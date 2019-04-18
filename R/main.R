2fee29c8a5768994daa50ca80c3913f696d3a296
Joseph Levesque <jwlevesque@gmail.com>
2019-04-18 12:42:29 -0400

    Initial Commit; Just the standard generated files for a new package, with hello.rd renamed to main.rd

---
+++ R/main.R
@@ -0,0 +1,13 @@
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+hello <- function() {
+  print("Hello, world!")
+}
