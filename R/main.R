d8618e0b77a9d2a4cc4b9798073a7aec702369ac
Joseph Levesque <jwlevesque@gmail.com>
2019-04-18 13:38:40 -0400

    Test Commit: Added method stub for isInputValid() and ran roxygen2.

--- R/main.R
+++ R/main.R
@@ -7,7 +7,20 @@
 #   Build and Reload Package:  'Ctrl + Shift + B'
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
+library(roxygen2)
+library(devtools)
 
-hello <- function() {
-  print("Hello, world!")
+#' Helper method for the main method.
+#'
+#' Tests to see if the input to the main function is valid.
+#' Valid input is defined as 2-5 data frames, each with:
+#' \itemize{
+#'     \item The same number of columns.
+#'     \item At least 2 unique values in the first column.
+#' }
+#'
+#' @param input A list() of the data frames passed as input to the main function.
+#' @return True if the input is valid, as defined above; false otherwise.
+isInputValid = function(input){
+  ## TO-DO: Method Stub
 }
